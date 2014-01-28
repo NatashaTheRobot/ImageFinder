29d61fb97d0194c12acf761e6ca3d5115963be58
Natasha Murashev <nmurashev@gmail.com>
2014-01-27 20:45:00 -0500

    spinners now displaying options

--- app/src/main/java/com/natashatherobot/app/FilterActivity.java
+++ app/src/main/java/com/natashatherobot/app/FilterActivity.java
@@ -10,15 +10,46 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class FilterActivity extends ActionBarActivity {
     QueryFilter filter;
+
+    EditText etSiteFilter;
+
+    Spinner sprColorFilter;
+    Spinner sprImageType;
+    Spinner sprImageSize;
+
+    ArrayAdapter<String> colorAdapter;
+    ArrayAdapter<String> typeAdapter;
+    ArrayAdapter<String> sizeAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter);
         filter = (QueryFilter) getIntent().getSerializableExtra("filter");
+
+        sprColorFilter = (Spinner) findViewById(R.id.sprColorFilter);
+        colorAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, filter.getColors());
+        colorAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sprColorFilter.setAdapter(colorAdapter);
+
+        sprImageType = (Spinner) findViewById(R.id.sprImageType);
+        typeAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, filter.getTypes());
+        typeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sprImageType.setAdapter(typeAdapter);
+
+        sprImageSize = (Spinner) findViewById(R.id.sprImageSize);
+        sizeAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, filter.getSizes());
+        sizeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sprImageSize.setAdapter(sizeAdapter);
+
+        etSiteFilter = (EditText) findViewById(R.id.etSiteFilter);
     }
 
 
--- app/src/main/java/com/natashatherobot/app/QueryFilter.java
+++ app/src/main/java/com/natashatherobot/app/QueryFilter.java
@@ -1,19 +1,27 @@
 package com.natashatherobot.app;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 /**
  * Created by Natasha Murashev on 1/25/14.
  */
 public class QueryFilter implements Serializable {
     private static final long serialVersionUID = 4219758740790527158L;
+
     private String size;
     private String color;
     private String type;
     private String site;
 
-    public QueryFilter() {
+    private ArrayList<String> sizes;
+    private ArrayList<String> colors;
+    private ArrayList<String> types;
 
+    public QueryFilter() {
+        configureSizes();
+        configureColors();
+        configureTypes();
     }
 
     public String getSize() {
@@ -46,5 +54,51 @@ public class QueryFilter implements Serializable {
 
     public void setSite(String site) {
         this.site = site;
+    }
+
+    public ArrayList<String> getColors() {
+        return colors;
+    }
+
+    public ArrayList<String> getSizes() {
+        return sizes;
+    }
+
+    public ArrayList<String> getTypes() {
+        return types;
+    }
+
+    private void configureColors() {
+        colors = new ArrayList<String>(11);
+        colors.add("black");
+        colors.add("blue");
+        colors.add("brown");
+        colors.add("gray");
+        colors.add("green");
+        colors.add("orange");
+        colors.add("pink");
+        colors.add("red");
+        colors.add("teal");
+        colors.add("white");
+        colors.add("yellow");
+    }
+
+    private void configureSizes() {
+        sizes = new ArrayList<String>(7);
+        sizes.add("icon");
+        sizes.add("small");
+        sizes.add("medium");
+        sizes.add("large");
+        sizes.add("xlarge");
+        sizes.add("xxlarge");
+        sizes.add("huge");
+    }
+
+    private void configureTypes() {
+        types = new ArrayList<String>(4);
+        types.add("clipart");
+        types.add("face");
+        types.add("lineart");
+        types.add("photo");
     }
 }
